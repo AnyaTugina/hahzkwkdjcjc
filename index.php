ebdbf6f776bf246eae50f42cdb0640bc1e56bbdd
AnyaTugina <sozdalacc468@gmail.com>
2025-01-25 17:22:32 +0300

    index.php

--- index.php
+++ index.php
@@ -158,7 +158,7 @@
         <h3>Доступные пасты:</h3>
 
         <?php
-        $pasteDir = '/';
+        $pasteDir = 'pastes/';
         $files = scandir($pasteDir);
 
         foreach ($files as $file) {
